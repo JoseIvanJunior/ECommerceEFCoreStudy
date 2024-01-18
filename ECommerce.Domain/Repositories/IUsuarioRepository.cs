a396c3b0ffd9046a18397e9e95d228e5d3036095
Jose Ivan Junior <joseivanjunior@gmail.com>
2024-01-18 00:37:32 -0300

    Add project files.

---
+++ ECommerce.Domain/Repositories/IUsuarioRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using ECommerce.Domain.Entities;
+
+namespace ECommerce.Domain.Repositories
+{
+    public interface IUsuarioRepository
+    {
+        List<Usuario> GetAll();
+        Usuario GetById(int id);
+        void Add(Usuario usuario);
+        void Update(Usuario usuario);
+        void Delete(int id);
+    }
+}
---
+++ ECommerce.Domain/Repositories/UsuarioRepository.cs
@@ -0,0 +1,35 @@
+﻿
+using ECommerce.Domain.Entities;
+
+namespace ECommerce.Domain.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        // Simulando um banco de dados para teste das funcionalidades
+        public static List<Usuario> _db = new List<Usuario>();
+        public List<Usuario> GetAll()
+        {
+            return _db;
+        }
+
+        public Usuario GetById(int id)
+        {
+            return _db.FirstOrDefault(x => x.Id == id)!;
+        }
+        public void Add(Usuario usuario)
+        {
+            _db.Add(usuario);
+        }
+
+        public void Update(Usuario usuario)
+        {
+            _db.Remove(GetById(usuario.Id));
+            _db.Add(usuario);
+        }
+
+        public void Delete(int id)
+        {
+            _db.Remove(GetById(id)); 
+        }
+    }
+}
